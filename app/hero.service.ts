d28cd7af04c3ee4cd612c3efb95c793c54d00ba6
xavierglab <xavier.glab@channelforge.com>
2016-03-07 16:48:16 +0000

    added mock delay on promise returning service functions to simulate low latency

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,5 +1,6 @@
 import {HEROES} from './mock-heroes';
 import {Injectable} from 'angular2/core';
+import {Hero} from './hero';
 
 @Injectable()
 
@@ -7,5 +8,18 @@ export class HeroService{
     getHeroes(){
         //return HEROES;
         return Promise.resolve(HEROES);        
+    }
+    
+    getHeroesSlowly(){
+        return new Promise<Hero[]>(resolve => 
+            setTimeout(() => resolve(HEROES), 5000)
+        );
+    }
+    getHeroesSuperSlowly(){
+        return new Promise<Hero[]>((resolve) => {
+            setTimeout(() => {
+                return resolve(HEROES);
+            }, 10000);
+        })
     }
 }
